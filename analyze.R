a645c9e9225d13aa7a6ffea1e77de6a3d8b6a082
Carl A. B. Pearson <blank101@gmail.com>
2020-09-04 13:02:17 -0400

    removes peak tags when below most recent resurgence threshold

--- analyze.R
+++ analyze.R
@@ -22,17 +22,57 @@ ref <- readRDS(rawpth)[
     total_tests_per_thousand
   ),
   keyby = .(iso_code, location, date)
-][7:.N]
+]
+
+# censor any leading NAs in new_cases_smoothed_per_million
+if (ref[1, is.na(new_cases_smoothed_per_million)]) ref <- ref[ 
+  which.max(!is.na(new_cases_smoothed_per_million)):.N
+]
 
 ref[, zz := ZigZag(new_cases_smoothed_per_million, 15) ]
-
 ref[, annotation := NA_character_ ]
+
+# assert: is.na(zz) is only at end of series
+if (ref[, any(is.na(zz))]) {
+  lastcertain <- ref[,which.max(is.na(zz))]-1
+  ref[
+    (lastcertain+1):.N,
+    c("zz","annotation") := .(
+      ref[lastcertain, zz],
+      "zz_NA"
+    )
+  ]
+}
 
 source(funspth)
 
 ref[find_uptick(new_cases_smoothed_per_million, len = 5), annotation := "uptick" ]
 ref[find_peaks(zz, m = 14), annotation := "peak" ]
 # ref[find_valleys(zz, m = 10, inclTail = FALSE), annotation := "valley"]
+
+newwave_threshold <- 1/3
+#' censor any peak annotations that are below new wave criteria
+#' for most recent wave
+#' b/c subsequent wave / resurgence peaks could be lower / higher
+ref[annotation == "peak", annotation := if (.N != 1) {
+  ind <- 1; keep <- rep(TRUE, .N)
+  while(ind < .N) {
+    reflvl <- new_cases_smoothed_per_million[ind]*newwave_threshold
+    slc <- new_cases_smoothed_per_million[(ind+1):.N] > reflvl
+    if (any(slc)) {
+      newind <- ind + which.max(slc)
+      if (ind + 1 != newind) keep[(ind+1):(newind-1)] <- FALSE
+      ind <- newind
+    } else {
+      keep[(ind+1):.N] <- FALSE
+      ind <- .N
+    }
+  }
+  print(keep)
+  newanno <- annotation
+  newanno[!keep] <- NA_character_
+  newanno
+} else "peak" ]
 
 # TODO: not quite there
 wave_end_thresholds <- ref[
