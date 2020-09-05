7b817de0c7e2791470eda5bb9030a8b08ee02eb9
Carl A. B. Pearson <blank101@gmail.com>
2020-09-05 14:11:32 -0400

    split annotations by point vs range, add uptick annotations

--- analyze.R
+++ analyze.R
@@ -4,7 +4,7 @@ suppressPackageStartupMessages({
   require(TTR)
 })
 
-.debug <- "FRA"
+.debug <- "KEN"
 .args <- if (interactive()) sprintf(c(
   "data/owid.rds", "featureFunctions.R", "%s", "results/%s/result.rds"
 ), .debug) else commandArgs(trailingOnly = TRUE)
@@ -29,14 +29,15 @@ if (ref[1, is.na(new_cases_smoothed_per_million)]) ref <- ref[
 ]
 
 ref[, zz := ZigZag(new_cases_smoothed_per_million, 15) ]
-ref[, annotation := NA_character_ ]
+ref[, point_annotation := NA_character_ ]
+ref[, range_annotation := NA_character_ ]
 
 # assert: is.na(zz) is only at end of series
 if (ref[, any(is.na(zz))]) {
   lastcertain <- ref[,which.max(is.na(zz))]-1
   ref[
     (lastcertain+1):.N,
-    c("zz","annotation") := .(
+    c("zz","range_annotation") := .(
       ref[lastcertain, zz],
       "zz_NA"
     )
@@ -46,19 +47,25 @@ if (ref[, any(is.na(zz))]) {
 source(funspth)
 
 #ref[find_uptick(new_cases_smoothed_per_million, len = 5), annotation := "uptick" ]
-ref[find_peaks(zz, m = 14, minVal = 1), annotation := "peak" ]
-first_peak_date <- ref[annotation == "peak"][1, date]
-ref[date > first_peak_date, annotation := ifelse(
-  find_upswing(new_cases_smoothed_per_million) & is.na(annotation),
-  "upswing", annotation
+ref[find_peaks(zz, m = 14, minVal = 1), point_annotation := "peak" ]
+first_peak_date <- ref[point_annotation == "peak"][1, date]
+ref[date > first_peak_date, range_annotation := ifelse(
+  find_upswing(new_cases_smoothed_per_million) & is.na(range_annotation),
+  "upswing", range_annotation
 )]
+ref[date > first_peak_date, point_annotation := ifelse(
+  find_upswing(new_cases_smoothed_per_million, 5, 5) & (is.na(point_annotation)),
+  "uptick", point_annotation
+)]
+
+
 # ref[find_valleys(zz, m = 10, inclTail = FALSE), annotation := "valley"]
 
 newwave_threshold <- 1/3
 #' censor any peak annotations that are below new wave criteria
 #' for most recent wave
 #' b/c subsequent wave / resurgence peaks could be lower / higher
-ref[annotation == "peak", annotation := if (.N != 1) {
+ref[point_annotation == "peak", point_annotation := if (.N != 1) {
   ind <- 1; keep <- rep(TRUE, .N)
   while(ind < .N) {
     reflvl <- new_cases_smoothed_per_million[ind]*newwave_threshold
@@ -72,14 +79,14 @@ ref[annotation == "peak", annotation := if (.N != 1) {
       ind <- .N
     }
   }
-  newanno <- annotation
+  newanno <- point_annotation
   newanno[!keep] <- NA_character_
   newanno
 } else "peak" ]
 
 # TODO: not quite there
 wave_end_thresholds <- ref[
-  annotation == "peak",
+  point_annotation == "peak",
   .(
     waveoff = new_cases_smoothed_per_million / 10,
     newwave = new_cases_smoothed_per_million / 3,
@@ -87,22 +94,40 @@ wave_end_thresholds <- ref[
   )
 ]
 
+# TODO: consider keeping annotation prior to first peak
 ref[
-  annotation %in% c("uptick", "upswing") & date < wave_end_thresholds[1, date],
-  annotation := NA_character_
+  range_annotation == "upswing" & date < wave_end_thresholds[1, date],
+  range_annotation := NA_character_
+]
+ref[
+  point_annotation == "uptick" & date < wave_end_thresholds[1, date],
+  point_annotation := NA_character_
 ]
 
 wave_end_thresholds[,{
   ref[
-    (date > wavedate) & (new_cases_smoothed_per_million < waveoff) & is.na(annotation),
-    annotation := "below"
+    (date > wavedate) & (new_cases_smoothed_per_million < waveoff) & is.na(range_annotation),
+    range_annotation := "below"
   ]
-  endwavedate <- ref[annotation == "below"][1, date]
-  ref[
-    (date > endwavedate) & (new_cases_smoothed_per_million > newwave) & (is.na(annotation) | (annotation != "peak")),
-    annotation := "above"
-  ]
+  # TODO: how does this work if there is no first below?
+  endwavedate <- ref[range_annotation == "below"][1, date]
+  if (!is.na(endwavedate)) {
+    ref[
+      (date > endwavedate) & (new_cases_smoothed_per_million > newwave),
+      range_annotation := "above"
+    ]
+    newwavedate <- ref[(date > endwavedate) & range_annotation == "above"][1, date]
+    if (!is.na(newwavedate)) ref[
+       between(date, endwavedate, newwavedate, incbounds = F) & is.na(range_annotation),
+       range_annotation := "post"
+    ]
+  }
 }, by=.(wavedate = date)]
+
+ref[range_annotation == "above" & point_annotation == "uptick", point_annotation := NA_character_ ]
+
+ref$range_annotation <- factor(ref$range_annotation, levels = c("below", "post", "upswing", "above"), ordered = TRUE)
+ref$point_annotation <- factor(ref$point_annotation, levels = c("peak", "uptick"), ordered = TRUE)
 
 #' TODO: from USA example:
 #'  - don't set / undo valley label if not in below territory
--- visualize.R
+++ visualize.R
@@ -4,9 +4,10 @@ suppressPackageStartupMessages({
   require(ggplot2)
 })
 
-.args <- if (interactive()) c(
-  "results/ESP/result.rds", "fig/ESP.png"
-) else commandArgs(trailingOnly = TRUE)
+.debug <- "FRA"
+.args <- if (interactive()) sprintf(c(
+  "results/%s/result.rds", "fig/%s.png"
+), .debug) else commandArgs(trailingOnly = TRUE)
 #' @example 
 #' .args <- gsub("ESP", "USA", .args)
 
@@ -15,13 +16,24 @@ targetfig <- tail(.args, 1)
 
 ref <- readRDS(rawpth)
 
+# TODO: consider if worth adding:
+#  - from peaks, draw post-wave threshold until reached / new peak
+#  - once post wave threshold reached, draw new wave threshold until reached
+
+# TODO: area plot version for shaded regions
+
 p <- ggplot(ref) + 
   aes(date, new_cases_smoothed_per_million) +
-#  geom_line(aes(y=zz, color = "zigzag")) +
-  geom_line(aes(color = "observed")) +
+  geom_bar(
+    aes(fill = range_annotation, color = NULL),
+    #data = function(dt) dt[!is.na(range_annotation)],
+    alpha = 0.5, width = 1, stat = "identity"
+  ) +
+  #  geom_line(aes(y=zz, color = "zigzag")) +
+  geom_line(color = "black") +
   geom_point(
-    aes(shape = annotation),
-    data = function(dt) dt[!is.na(annotation) & !(annotation %in% c("below"))]
+    aes(shape = point_annotation)#,
+    #data = function(dt) dt[point_annotation %in% c("peak", "uptick")]
   ) +
   scale_x_date(
     "Date",
@@ -31,13 +43,27 @@ p <- ggplot(ref) +
   ) +
   scale_y_continuous("reported 7-day mean case incidence per 1M") +
   scale_shape_manual(
-    NULL,
-    values = c(peak=17, above=24, upswing=23)
+    "Point Indicators",
+    breaks = levels(ref$point_annotation),
+    values = c(peak=17, uptick=23),
+    drop = FALSE
   ) +
-  scale_color_manual(
-    NULL,
-    labels = c(observed = "Reported", zigzag = "ZigZag Indicator"),
-    values = c(observed = "black", zigzag = "firebrick")
+  scale_fill_manual(
+    "Range Indicators",
+    breaks = levels(ref$range_annotation),
+    labels = c(
+      above = "above 1/3rd",
+      below = "below 1/10th",
+      post = "between 1/10th & 1/3rd, not upswing",
+      upswing = "upswing"
+    ),
+    values = c(
+      above = "firebrick",
+      upswing = "orange",
+      below = "dodgerblue",
+      post = "yellow"
+    ),
+    drop = F
   ) +
   theme_minimal() +
   theme(
